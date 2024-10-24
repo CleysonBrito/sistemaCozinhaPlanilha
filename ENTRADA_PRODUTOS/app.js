1cf12583b392a8d4190582a5bf87c0bfaff05c41
CleysonBrito <cleyson@reciclar.org.br>
2024-10-24 12:35:57 -0300

    caminho

--- ENTRADA_PRODUTOS/app.js
+++ ENTRADA_PRODUTOS/app.js
@@ -27,9 +27,9 @@ document.getElementById('dataForm').addEventListener('submit', (e) => {
         data_vencimento
     };
 
-    fetch("https://script.google.com/macros/s/AKfycbxM2WNFSr9VZyDibR8H4SQhApM-8t3tNFSdMP4zj_hIETjoegzudMHvVttrz8MTRS9gUA/exec", {
+    fetch("https://institutoreciclar-my.sharepoint.com/:x:/r/personal/admin_institutoreciclar_onmicrosoft_com/_layouts/15/Doc.aspx?sourcedoc=%7BDE2C17EA-D84F-4BFF-B8E9-15753D74364D%7D&file=Pasta.xlsx&action=editnew&mobileredirect=true&wdNewAndOpenCt=1729782763717&ct=1729782764785&wdOrigin=OFFICECOM-WEB.START.NEW&wdPreviousSessionSrc=HarmonyWeb&wdPreviousSession=ddd753a8-14c1-4777-9f7b-ce5ef8537aea&cid=8ef89d60-b0f4-4eaf-a0b1-877b476ddb1a", {
         method: "POST",
-        mode: "cors",  // Garantir que a requisição seja feita com o modo CORS
+        mode: "cors",
         headers: {
             "Content-Type": "application/json"
         },
@@ -46,7 +46,6 @@ document.getElementById('dataForm').addEventListener('submit', (e) => {
         console.error('Erro ao salvar produto: ', error);
     });
 });
-
 
 // Função para calcular o valor total
 function calcularValorTotal() {
@@ -65,4 +64,3 @@ function limparFormulario() {
 function irParaHome() {
     window.location.href = "./home.html"; // Substitua pelo caminho correto para a página inicial
 }
-
